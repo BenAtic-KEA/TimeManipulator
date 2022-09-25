a1e68e1d2c0e732b8e398f62d6f5d1f78afa3b2c
Mark Hansen <Mark024e@stud.kea.dk>
2022-09-25 13:35:36 +0200

    changes to work on netlify

--- app.js
+++ app.js
@@ -4,34 +4,8 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
-const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-const date = new Date();
-let day = days[date.getDay()];
-let month = months[date.getMonth()];
-let dayMonth =  `${date.getDate()}/${date.getMonth() + 1}`;
-let year = date.getFullYear();
-/*
-res.send({
-    date: `${date}/${month + 1}`,
-    today: days[todaysday],
-    month: months[month],
-    year: year
-    
-})
-*/
-
 app.get("/",(req,res) =>{
-    res.sendFile(path.resolve("./public/frontpage.html"))
+    res.sendFile(path.resolve("./public/index.html"))
 } )
-
-app.get("/today", (req,res) => {
-    res.send({
-        date: `${date.getDate()}/${date.getMonth() + 1}`,
-        today: day,
-        month: month,
-        year: year
-    })
-})
 
 app.listen(8080,() => console.log("server is running on port",8080));
