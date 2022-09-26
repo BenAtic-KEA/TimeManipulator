c715b7f161be88c5bf99afe5ffb72ebca7480e74
Mark Hansen <Mark024e@stud.kea.dk>
2022-09-26 14:01:54 +0200

    fixed a sunday bug with week number

--- public/frontpage.js
+++ public/frontpage.js
@@ -1,7 +1,6 @@
 const actualDay = document.getElementById("today")
 const weekday = document.getElementById("weeknr")
 
-
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 const date = new Date();
@@ -13,7 +12,12 @@ let year = date.getFullYear();
 function getWeekNumber(){
     let startDate = new Date(date.getFullYear(),0,1);
     let days = Math.floor((date - startDate) / (24 * 60 * 60 * 1000));
-    let weekNumber = Math.floor(days / 7);
+    let weekNumber;
+    if(date.getDay() === 0){
+        weekNumber = Math.floor(days / 7);
+    return weekNumber
+    }
+        weekNumber = Math.ceil(days / 7);
     return weekNumber
 }
 
